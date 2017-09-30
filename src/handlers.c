59b33010848634cb8b3e5c91ac72d4bb69aae7b4
Daniel Mueller <deso@posteo.net>
2017-09-30 09:16:27 -0700

    i3bar: Draw indication whether workspaces have windows or not
    Awesome has the nice feature that it will indicate on each workspace
    whether it contains windows or not by drawing a small rectangle along
    with the workspace indication in the bar.
    This change sets out to make i3 behave the same way. To accomplish this
    task we introduce a new property, 'has_children', to the workspaces IPC
    command that is indicative of whether a workspace has windows or not. We
    then adjust i3 to inform IPC clients of workspace changes whenever a new
    windows is created, destroyed, or moved to a different workspace.

--- src/handlers.c
+++ src/handlers.c
@@ -287,6 +287,8 @@ static void handle_map_request(const xcb_map_request_event_t *event) {
     add_ignore_event(event->sequence, -1);
 
     manage_window(event->window, cookie, false);
+    ipc_send_workspace_event("reload", NULL, NULL);
+    return;
 }
 
 /*
@@ -517,6 +519,7 @@ static void handle_unmap_notify_event(xcb_unmap_notify_event_t *event) {
 
     tree_close_internal(con, DONT_KILL_WINDOW, false);
     tree_render();
+    ipc_send_workspace_event("reload", NULL, NULL);
 
 ignore_end:
     /* If the client (as opposed to i3) destroyed or unmapped a window, an
