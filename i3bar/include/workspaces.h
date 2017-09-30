dfb7ab98c6275f888e42bdc01d2d2c20f5cdef7e
Daniel Mueller <deso@posteo.net>
2017-09-30 09:16:27 -0700

    i3bar: Draw indication whether workspaces have windows or not
    Awesome has the nice feature that it will indicate on each workspace
    whether it contains windows or not by drawing a small rectangle along
    with the workspace indication in the bar.
    This change sets out to make i3 behave the same way. To accomplish this
    task we introduce a new property, 'has_children', to the workspaces IPC
    command that is indicative of whether a workspace has windows or not. We
    then adjust i3 to inform IPC clients of workspace changes whenever a new
    windows is created, destroyed, or moved to a different workspace.

--- i3bar/include/workspaces.h
+++ i3bar/include/workspaces.h
@@ -38,6 +38,7 @@ struct i3_ws {
     bool visible;             /* If the ws is currently visible on an output */
     bool focused;             /* If the ws is currently focused */
     bool urgent;              /* If the urgent hint of the ws is set */
+    bool has_children;        /* If the current ws has children (i.e, windows) */
     struct i3_output *output; /* The current output of the ws */
 
     TAILQ_ENTRY(i3_ws) tailq; /* Pointer for the TAILQ-Macro */
