8639ac2c7b5af985962b00956958735b73b0ca1e
Daniel Müller <deso@posteo.net>
2025-12-01 21:40:53 -0800

    Number workspaces using base 36
    Number workspaces using base 36 instead of 10. In so doing we have more
    single letter workspace names, navigation to which can intuitively be
    bound to the corresponding keys.

--- src/randr.c
+++ src/randr.c
@@ -853,6 +853,7 @@ static void move_content(Con *con) {
      * of floating containers as we go */
     Con *current;
     Con *old_content = output_get_content(con);
+    char num_str[3];
     while (!TAILQ_EMPTY(&(old_content->nodes_head))) {
         current = TAILQ_FIRST(&(old_content->nodes_head));
         DLOG("Detaching current = %p / %s\n", current, current->name);
@@ -861,7 +862,7 @@ static void move_content(Con *con) {
         /* Adjust the con's number to make it be sorted in properly. */
         current->num = ++num_children;
         free(current->name);
-        sasprintf(&current->name, "%d", current->num);
+        current->name = strdup(num_to_base36(current->num, num_str, sizeof(num_str)));
         con_attach(current, first_content, false);
         DLOG("Fixing the coordinates of floating containers\n");
         Con *floating_con;
@@ -897,7 +898,7 @@ static void move_content(Con *con) {
             /* Adjust the con's number to make it be sorted in properly. */
             dock->num = ++num_children;
             free(dock->name);
-            sasprintf(&dock->name, "%d", dock->num);
+            dock->name = strdup(num_to_base36(dock->num, num_str, sizeof(num_str)));
             con_attach(dock, nc, false);
             DLOG("Done\n");
         }
